f12972f85f71786ecb4ad53e6283b383ac954553
MATT BRUTON <the.bruton@gmail.com>
2016-11-27 20:04:00 -0600

    changed navigation styling, responsive

--- src/components/nav/nav-component.jsx
+++ src/components/nav/nav-component.jsx
@@ -7,7 +7,7 @@ export default class Navbar extends React.Component {
   render() {
 
     return (
-      <nav className="col-md-4 offset-md-8 col-sm-12 text-xs-center navbar-fixed-top navbar__container">
+      <nav className="col-sm-12 text-xs-center text-md-right navbar-fixed-top navbar__container">
         <ul className="nav nav-inline nav__container--mobile">
           <NavItem url="#" text="about"/>
           <NavItem url="#" text="projects"/>
